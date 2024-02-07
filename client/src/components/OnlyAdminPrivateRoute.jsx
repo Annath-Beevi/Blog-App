afc28034929ba51f2e6ec611d6bd2950a72ef33e
Annath Beevi <annathbeevi01@gmail.com>
2024-02-07 21:33:33 +0530

    complete create post functionality

--- client/src/components/OnlyAdminPrivateRoute.jsx
+++ client/src/components/OnlyAdminPrivateRoute.jsx
@@ -4,7 +4,6 @@ import { Navigate, Outlet } from 'react-router-dom'
 
 export default function OnlyAdminPrivateRoute() {
     const {currentUser} = useSelector((state) => state.user)
-    console.log(currentUser);
   return (
     currentUser && currentUser.isAdmin ? <Outlet/> : <Navigate to='/sign-in'/>
   )
