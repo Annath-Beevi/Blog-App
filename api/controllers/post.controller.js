be5976e39a3f18ab98879a57bfb83494bc0140bf
Annath Beevi <annathbeevi01@gmail.com>
2024-02-09 20:59:50 +0530

    add update post functionality

--- api/controllers/post.controller.js
+++ api/controllers/post.controller.js
@@ -28,36 +28,36 @@ export const getPosts = async (req, res, next) => {
         const limit = parseInt(req.query.limit) || 9;
         const sortDirection = req.query.order === 'asc' ? 1 : -1;
         const posts = await Post.find({
-            ...(req.query.userId && {userId: req.query.userId}),
-            ...(req.query.category && {category: req.query.category}),
-            ...(req.query.slug && {category: req.query.slug}),
-            ...(req.query.postId && {_id: req.query.postId}),
+            ...(req.query.userId && { userId: req.query.userId }),
+            ...(req.query.category && { category: req.query.category }),
+            ...(req.query.slug && { category: req.query.slug }),
+            ...(req.query.postId && { _id: req.query.postId }),
             ...(req.query.searchTerm && {
                 $or: [
-                    {title: { $regex: req.query.searchTerm, $options: 'i'}},
-                    {content: { $regex: req.query.searchTerm, $options: 'i'}},
+                    { title: { $regex: req.query.searchTerm, $options: 'i' } },
+                    { content: { $regex: req.query.searchTerm, $options: 'i' } },
                 ],
             }),
-    }).sort({ updatedAt: sortDirection}).skip(startIndex).limit(limit)
-    const totalPosts = await Post.countDocuments()
+        }).sort({ updatedAt: sortDirection }).skip(startIndex).limit(limit)
+        const totalPosts = await Post.countDocuments()
 
-    const now = new Date();
-    
-    const oneMonthAgo = new Date(
-        now.getFullYear(),
-        now.getMonth() - 1,
-        now.getDate()
-    )
+        const now = new Date();
 
-    const lastMonthPosts = await Post.countDocuments({
-        createdAt: { $gte: oneMonthAgo}
-    })
+        const oneMonthAgo = new Date(
+            now.getFullYear(),
+            now.getMonth() - 1,
+            now.getDate()
+        )
 
-    res.status(200).json({
-        posts,
-        totalPosts,
-        lastMonthPosts
-    })
+        const lastMonthPosts = await Post.countDocuments({
+            createdAt: { $gte: oneMonthAgo }
+        })
+
+        res.status(200).json({
+            posts,
+            totalPosts,
+            lastMonthPosts
+        })
 
     } catch (error) {
         next(error)
@@ -65,13 +65,35 @@ export const getPosts = async (req, res, next) => {
 }
 
 export const deletePost = async (req, res, next) => {
-    if(!req.user.isAdmin || req.user.id !== req.params.userId) {
+    if (!req.user.isAdmin || req.user.id !== req.params.userId) {
         return next(errorHandler(403, 'You are not allowed to delete this post'))
     }
-    try{
+    try {
         await Post.findByIdAndDelete(req.params.postId);
         res.status(200).json('The post has been deleted')
-    }catch (error) {
+    } catch (error) {
+        next(error)
+    }
+}
+
+export const updatedPost = async (req, res, next) => {
+    if (!req.user.isAdmin || req.user.id !== req.params.userId) {
+        return next(errorHandler(403, 'You are not allowed to update this post'))
+    }
+    try {
+        const updatedPost = await Post.findByIdAndUpdate(
+            req.params.postId,
+            {
+                $set: {
+                    title: req.body.title,
+                    content: req.body.content,
+                    category: req.body.category,
+                    image: req.body.image,
+                }
+            }, { new: true }
+        );
+        res.status(200).json(updatedPost)
+    } catch (error) {
         next(error)
     }
 }
