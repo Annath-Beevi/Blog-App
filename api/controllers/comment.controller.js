7f4a33e83c84150ba73443efc15cb718b401857b
Annath Beevi <annathbeevi01@gmail.com>
2024-02-19 20:26:08 +0530

    add like functionality to the comment component

--- api/controllers/comment.controller.js
+++ api/controllers/comment.controller.js
@@ -2,10 +2,10 @@ import { errorHandler } from "../utils/error.js";
 import Comment from '../models/comment.model.js'
 
 export const createComment = async (req, res, next) => {
-    try{
-        const {content, postId, userId} = req.body;
+    try {
+        const { content, postId, userId } = req.body;
 
-        if(userId !== req.user.id){
+        if (userId !== req.user.id) {
             return next(errorHandler(403, 'You are not allowed to create this comment'))
         }
 
@@ -17,17 +17,38 @@ export const createComment = async (req, res, next) => {
         await newComment.save()
         res.status(200).json(newComment)
 
-    }catch(error) {
+    } catch (error) {
         next(error)
     }
-} 
+}
 
-export const getPostComments = async (req,res, next) => {
+export const getPostComments = async (req, res, next) => {
     try {
-        const comments = await Comment.find({postId: req.params.postId}).sort({
-            createdAt : -1
+        const comments = await Comment.find({ postId: req.params.postId }).sort({
+            createdAt: -1
         })
         res.status(200).json(comments)
+    } catch (error) {
+        next(error)
+    }
+}
+
+export const likeComment = async (req, res, next) => {
+    try {
+        const comment = await Comment.findById(req.params.commentId)
+        if(!comment){
+            return next(errorHandler(404, 'Comment not found'))
+        }
+        const userIndex = comment.likes.indexOf(req.user.id);
+        if(userIndex === -1){
+            comment.numberOfLikes += 1;
+            comment.likes.push(req.user.id)
+        } else {
+            comment.numberOfLikes -= 1;
+            comment.likes.splice(userIndex, 1)
+        }
+        await comment.save()
+        res.status(200).json(comment)
     } catch (error) {
         next(error)
     }
--- client/src/components/Comment.jsx
+++ client/src/components/Comment.jsx
@@ -1,23 +1,27 @@
 import React, { useEffect, useState } from 'react'
 import moment from 'moment'
+import {FaThumbsUp} from 'react-icons/fa'
+import { useSelector } from 'react-redux'
 
-export default function Comment({comment}) {
+export default function Comment({comment, onLike}) {
     const [user, setUser] = useState({})
-    console.log(user);
+    const {currentUser} = useSelector((state) => state.user)
+
     useEffect(() => {
         const getUser = async () => {
-            try {
-                const res = await fetch(`/api/user/${comment.userId}`)
-                const data = await res.json();
-                if(res.ok){
-                    setUser(data)
-                }
-            } catch (error) {
-                console.log(error.message);
+          try {
+            const res = await fetch(`/api/user/${comment.userId}`);
+            const data = await res.json();
+            if (res.ok) {
+              setUser(data);
             }
-        }
-        getUser()
-    },[comment])
+          } catch (error) {
+            console.log(error.message);
+          }
+        };
+        getUser();
+      }, [comment]);
+
   return (
     <div className="flex p-4 border-b dark:border-gray-600 text-sm">
         <div className="flex-shrink-0 mr-3">
@@ -31,6 +35,17 @@ export default function Comment({comment}) {
                 </span>
             </div>
             <p className='text-gray-500 mb-2'>{comment.content}</p>
+            <div className="flex items-center pt-2 text-xs border-t dark:border-gray-500 max-w-fit gap-2">
+                <button type='button' onClick={() => onLike(comment._id)} className={`text-gray-400 hover:text-blue-500 
+                ${currentUser && comment.likes.includes(currentUser._id) && '!text-blue-500'}`}>
+                    <FaThumbsUp className='text-sm'/>
+                </button>
+                <p className='text-gray-400'>
+                  {
+                    comment.numberOfLikes > 0 && comment.numberOfLikes + " " + (comment.numberOfLikes === 1 ? "like" : "likes")
+                  }
+                </p>
+            </div>
         </div>
     </div>
   )
